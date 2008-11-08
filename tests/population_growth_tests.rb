ff37f51db673c45bdc3ca82e400951f935723860
Josh Adams <jadams@goro.(none)>
2008-11-08 11:28:36 -0600

    initial checkin

---
+++ tests/population_growth_tests.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class PopulationGrowthTests < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_fail
+    assert(false, 'Assertion was false.')
+  end
+end
---
+++ tests/suite.rb
@@ -0,0 +1,2 @@
+require 'test/unit/testsuite'
+require 'population_growth_tests'
